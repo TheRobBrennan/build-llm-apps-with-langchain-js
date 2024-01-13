2b67c8d4b5c850fd4a259928f4976837a76f8864
Rob Brennan <rob@therobbrennan.com>
2024-01-13 15:09:48 -0800

    Load documents from a GitHub repository using a LangChain Document Loader

--- src/lesson-2/main.ts
+++ src/lesson-2/main.ts
@@ -1,14 +1,22 @@
-// --------------------------------------------------------------------------
-// Lesson 2: Loading and preparing data
-// --------------------------------------------------------------------------
+// Load data from a GitHub repository
+import { GithubRepoLoader } from "langchain/document_loaders/web/github";
+import ignore from "ignore"; // Peer dependency, used to support .gitignore syntax
 
 // --------------------------------------------------------------------------
-// Load environment variables from our .env file
+// Load documents from a GitHub repository using a LangChain Document Loader
 // --------------------------------------------------------------------------
+const GITHUB_REPOSITORY_URL = "https://github.com/langchain-ai/langchainjs";
+const GITHUB_IGNORED_PATHS = ["*.md", "yarn.lock"];
 
-// --------------------------------------------------------------------------
-// Loading
-// --------------------------------------------------------------------------
+// Will not include anything under "ignorePaths" - and recursion is turned off for demo purposes
+const loader = new GithubRepoLoader(
+  GITHUB_REPOSITORY_URL,
+  { recursive: false, ignorePaths: GITHUB_IGNORED_PATHS },
+);
+
+const docs = await loader.load();
+
+console.log(docs.slice(0, 3));
 
 // --------------------------------------------------------------------------
 // Splitting
