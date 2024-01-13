af950cd411c5b9afda9840e599f40133a1011484
Rob Brennan <rob@therobbrennan.com>
2024-01-13 15:32:39 -0800

    Load PDF from a local folder using a LangChain PDF Loader

--- src/lesson-2/main.ts
+++ src/lesson-2/main.ts
@@ -2,6 +2,11 @@
 import { GithubRepoLoader } from "langchain/document_loaders/web/github";
 import ignore from "ignore"; // Peer dependency, used to support .gitignore syntax
 
+// Load PDF from a local folder
+// Peer dependency
+import * as parse from "pdf-parse";
+import { PDFLoader } from "langchain/document_loaders/fs/pdf";
+
 // --------------------------------------------------------------------------
 // Load documents from a GitHub repository using a LangChain Document Loader
 // --------------------------------------------------------------------------
@@ -17,6 +22,16 @@ const loader = new GithubRepoLoader(
 const docs = await loader.load();
 
 console.log(docs.slice(0, 3));
+
+// --------------------------------------------------------------------------
+// Load PDF from a local folder using a LangChain PDF Loader
+// --------------------------------------------------------------------------
+const pdfLoader = new PDFLoader(
+  "src/lesson-2/data/machine-learning.pdf",
+);
+const rawCS229Docs = await pdfLoader.load();
+
+console.log(rawCS229Docs.slice(0, 5));
 
 // --------------------------------------------------------------------------
 // Splitting
