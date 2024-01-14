e2d2a335698b65430ac2ecaae0bb1b8295d7ae9d
Rob Brennan <rob@therobbrennan.com>
2024-01-13 17:24:04 -0800

    Create a Loader that will read in environment variables from our .env file

--- src/lesson-3/main.ts
+++ src/lesson-3/main.ts
@@ -1,1 +1,4 @@
-console.log(`Hello, lesson 3!\n`);
+import "../loaders/env_loader.ts";
+
+// TEST: Print our environment variables
+console.log(`OPENAI_API_KEY: ${Deno.env.get("OPENAI_API_KEY")}\n`);
