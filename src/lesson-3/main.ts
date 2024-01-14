1a9dc350405015da49f787bc85ca939a2d7bf1f2
Rob Brennan <rob@therobbrennan.com>
2024-01-13 23:09:58 -0800

    EXAMPLE: Let's compare vectors and see how similar they are

--- src/lesson-3/main.ts
+++ src/lesson-3/main.ts
@@ -1,14 +1,49 @@
 import "../loaders/env_loader.ts";
 import { OpenAIEmbeddings } from "npm:@langchain/openai";
+import { similarity } from "npm:ml-distance";
 
 // TEST: Print our environment variables
 console.log(`OPENAI_API_KEY: ${Deno.env.get("OPENAI_API_KEY")}\n\n`);
 
+// --------------------------------------------------------------------------
 // Vectorstore ingestion
+// --------------------------------------------------------------------------
 const embeddings = new OpenAIEmbeddings();
 
-// What do our embeddings look like?
+// --------------------------------------------------------------------------
+// EXAMPLE: What do our embeddings look like?
+// --------------------------------------------------------------------------
 const embeddingsResponse = await embeddings.embedQuery(
   "This is some sample text",
 );
 console.log(embeddingsResponse);
+
+// --------------------------------------------------------------------------
+// EXAMPLE: Let's compare vectors and see how similar they are
+// --------------------------------------------------------------------------
+const vector1 = await embeddings.embedQuery(
+  "What are vectors useful for in machine learning?",
+);
+
+const unrelatedVector = await embeddings.embedQuery(
+  "A group of parrots is called a pandemonium.",
+);
+
+const similarVector = await embeddings.embedQuery(
+  "Vectors are representations of information.",
+);
+
+// --------------------------------------------------------------------------
+// Calculate the cosine similarity between two vectors
+// --------------------------------------------------------------------------
+const unrelatedVectorSimilarityScore = similarity.cosine(
+  vector1,
+  unrelatedVector,
+);
+const relatedVectorSimilarityScore = similarity.cosine(vector1, similarVector);
+
+console.log(
+  "Unrelated vector similarity score: ",
+  unrelatedVectorSimilarityScore,
+);
+console.log("Related vector similarity score: ", relatedVectorSimilarityScore);
