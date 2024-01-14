5498efdca6efbeb2779d2054e06c91f8d1e15868
Rob Brennan <rob@therobbrennan.com>
2024-01-13 23:01:03 -0800

    Let's look at sample vector embeddings

--- src/lesson-3/main.ts
+++ src/lesson-3/main.ts
@@ -1,4 +1,14 @@
 import "../loaders/env_loader.ts";
+import { OpenAIEmbeddings } from "npm:@langchain/openai";
 
 // TEST: Print our environment variables
-console.log(`OPENAI_API_KEY: ${Deno.env.get("OPENAI_API_KEY")}\n`);
+console.log(`OPENAI_API_KEY: ${Deno.env.get("OPENAI_API_KEY")}\n\n`);
+
+// Vectorstore ingestion
+const embeddings = new OpenAIEmbeddings();
+
+// What do our embeddings look like?
+const embeddingsResponse = await embeddings.embedQuery(
+  "This is some sample text",
+);
+console.log(embeddingsResponse);
